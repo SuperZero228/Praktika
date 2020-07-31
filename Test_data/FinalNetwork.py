a4e8c55bab11f1856801459acfaedf855827836f
SuperZero228 <50049165+SuperZero228@users.noreply.github.com>
2020-07-31 16:27:33 +0300

    День 5. Описали архитектуру сети

--- Test_data/FinalNetwork.py
+++ Test_data/FinalNetwork.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import os
-import cv2 as cv
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -25,31 +24,36 @@ def load_and_preprocess_image(path):
   image = tf.io.read_file(path)
   return preprocess_image(image)
 
+def check(image_ds):
+    import matplotlib.pyplot as plt
+
+    for n, image in enumerate(image_ds.take(2)):
+      fig, ax = plt.subplots()
+      ax.imshow(image)
+      fig.set_figwidth(6)  # ширина и
+      fig.set_figheight(6)  # высота "Figure"
+      plt.show()
+
+
+
 path_source_ds = tf.data.Dataset.from_tensor_slices(all_source_image_paths)
 source_image_ds = path_source_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-
-import matplotlib.pyplot as plt
-for n, image in enumerate(source_image_ds.take(2)):
-  fig, ax = plt.subplots()
-  ax.imshow(image)
-  fig.set_figwidth(6)  # ширина и
-  fig.set_figheight(6)  # высота "Figure"
-  plt.show()
-
 
 path_marked_ds = tf.data.Dataset.from_tensor_slices(all_marked_image_paths)
 marked_image_ds = path_marked_ds.map(load_and_preprocess_image,  num_parallel_calls=AUTOTUNE)
 
-for n, image in enumerate(marked_image_ds.take(2)):
-  fig, ax = plt.subplots()
-  ax.imshow(image)
-  fig.set_figwidth(6)  # ширина и
-  fig.set_figheight(6)  # высота "Figure"
-  plt.show()
+#check(marked_image_ds)
 
-final_ds = tf.data.Dataset.zip((path_source_ds, path_marked_ds))
-
+final_ds = tf.data.Dataset.zip((source_image_ds, marked_image_ds))
 print(final_ds)
+
+
+# source_image_ds, marked_image_ds = final_ds
+# check(marked_image_ds)
+
+
+
+
 
 BATCH_SIZE = 1
 
@@ -61,6 +65,30 @@ ds = ds.batch(BATCH_SIZE)
 # `prefetch` позволяет датасету извлекать пакеты в фоновом режиме, во время обучения модели.
 ds = ds.prefetch(buffer_size=AUTOTUNE)
 print(ds)
-#Потом
-steps_per_epoch=tf.math.ceil(len(all_image_paths)/BATCH_SIZE).numpy()
-model.fit(ds, epochs=1, steps_per_epoch=3)
+
+model = tf.keras.Sequential([
+    #encoder
+    tf.keras.Input(shape=(720, 480, 3), name="Input"),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=5, strides=1, activation='relu', padding="same"),
+    tf.keras.layers.MaxPooling2D(pool_size=2, padding='same'),
+    tf.keras.layers.Conv2D(filters=8, kernel_size=3, strides=1, activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling2D(pool_size=2, padding='same'),
+
+    #decoder
+    tf.keras.layers.Conv2D(filters=8,  kernel_size=3, strides=1, activation='relu', padding='same'),
+    tf.keras.layers.UpSampling2D(size=2),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', padding='same'),
+    tf.keras.layers.UpSampling2D(size=2),
+    tf.keras.layers.Conv2D(filters=1, kernel_size=3, strides=1, activation='sigmoid', padding='same')
+])
+
+model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+
+steps_per_epoch=tf.math.ceil(len(all_source_image_paths)/BATCH_SIZE).numpy()
+print(steps_per_epoch)
+
+model.fit(ds, epochs=1000, steps_per_epoch=2, batch_size=1)
+model.evaluate(ds)
