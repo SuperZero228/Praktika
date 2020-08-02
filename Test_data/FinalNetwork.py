d5a35bf1fdcfa54fb939e9c63fa6cdc173894de8
SuperZero228 <50049165+SuperZero228@users.noreply.github.com>
2020-08-02 14:04:47 +0300

    День 7. Проверили работу модели

--- Test_data/FinalNetwork.py
+++ Test_data/FinalNetwork.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import os
+import numpy as np
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -19,6 +20,8 @@ def preprocess_image(image):
   image = tf.image.decode_jpeg(image)
   image = tf.image.resize(image, [720, 480])
   image /= 255.0  # normalize to [0,1] range
+  # image *= 2
+  # image -= 1
   return image
 
 def load_and_preprocess_image(path):
@@ -28,7 +31,7 @@ def load_and_preprocess_image(path):
 def check(image_ds):
     import matplotlib.pyplot as plt
 
-    for n, image in enumerate(image_ds.take(2)):
+    for n, image in enumerate(image_ds.take(42)):
       fig, ax = plt.subplots()
       ax.imshow(image)
       fig.set_figwidth(6)  # ширина и
@@ -39,10 +42,14 @@ def check(image_ds):
 
 path_source_ds = tf.data.Dataset.from_tensor_slices(all_source_image_paths)
 source_image_ds = path_source_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+print(source_image_ds)
 
 path_marked_ds = tf.data.Dataset.from_tensor_slices(all_marked_image_paths)
 marked_image_ds = path_marked_ds.map(load_and_preprocess_image,  num_parallel_calls=AUTOTUNE)
-
+print(marked_image_ds)
+print(len(list(marked_image_ds.as_numpy_iterator())))
+#
+# check(source_image_ds)
 
 final_ds = tf.data.Dataset.zip((source_image_ds, marked_image_ds))
 print(final_ds)
@@ -55,7 +62,7 @@ print(final_ds)
 
 
 
-BATCH_SIZE = 5
+BATCH_SIZE = 10
 
 # Установка размера буфера перемешивания, равного набору данных, гарантирует
 # полное перемешивание данных.
@@ -79,31 +86,34 @@ model = tf.keras.Sequential([
     tf.keras.layers.UpSampling2D(size=2),
     tf.keras.layers.Conv2D(filters=16, kernel_size=3, strides=1, activation='relu', padding='same'),
     tf.keras.layers.UpSampling2D(size=2),
-    tf.keras.layers.Conv2D(filters=1, kernel_size=3, strides=1, activation='sigmoid', padding='same')
+    tf.keras.layers.Conv2D(filters=1, kernel_size=3, strides=1, activation='sigmoid', padding='same'),
+    #tf.keras.layers.
 ])
 
-model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+loss = tf.keras.losses
+model.compile(optimizer='Adam',
+              loss=loss.cosine_similarity,
+              metrics=['accuracy'])
 
 
 # Распечатаем архитектуру модели
 model.summary()
+
+print(model.activity_regularizer)
 
 # Создаем коллбек сохраняющий веса модели
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=CHECKPOINT_PATH,
                                                  save_weights_only=True,
                                                  verbose=2)
 
-#x_train, y_train = ds
 
-
-
-model.fit(ds, epochs=40, steps_per_epoch=5, callbacks=[cp_callback], batch_size=10)
+model.fit(ds, epochs=1000, steps_per_epoch=5, callbacks=[cp_callback])
 model.evaluate(ds, steps=10)
 
 
 # Сохраним всю модель в  HDF5 файл
 model.save('Weights/my_model.h5')
+model.save('Weights/my_model')
 # Сохраняем веса
 model.save_weights('Weights/check/my_checkpoint_weights')
+
--- Test_data/test.py
+++ Test_data/test.py
@@ -1,18 +1,66 @@
 import cv2 as cv
+import tensorflow as tf
+import numpy as np
 
-model = cv.dnn.readNetFromTensorflow('Weights/my_model.h5')
+def preprocess_image(image):
+  image = tf.image.decode_jpeg(image)
+  image = tf.image.resize(image, [720, 480])
+  image /= 255.0  # normalize to [0,1] range
+  image *= 2
+  image -= 1
+  return image
 
-img = cv.imread("Finale/Source/1538647057_1-2.jpg")
+def load_and_preprocess_image(path):
+  image = tf.io.read_file(path)
+  return preprocess_image(image)
 
-#cv.imshow("1", img)
 
-# Use the given image as input, which needs to be blob(s).
-img
-blob = cv.dnn.blobFromImage(img, size=(720, 480), ddepth=cv.CV_32F, swapRB=True, crop=False)
-model.setInput(blob)
-# Runs a forward pass to compute the net output
-out = model.forward()
-print(1)
-cv.imshow("3", out)
+print(tf.train.list_variables('Weights/check/my_checkpoint_weights'))
 
-cv.waitKey(0)
+
+
+model = tf.keras.models.load_model('Weights/my_model.h5')
+print(model.get_weights())
+image = load_and_preprocess_image("Finale/Source/1.jpg")
+# print(image)
+np = np.array([image])
+print(np)
+out = model.predict(np)
+out = out[0]
+out += 1
+out /= 2
+out *= 255
+print(out)
+cv.imshow("1", out)
+cv.waitKey()
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.imshow(out)
+fig.set_figwidth(6)  # ширина и
+fig.set_figheight(6)  # высота "Figure"
+plt.show()
+
+
+
+
+# # Надо как то конвертировать
+# cv.dnn.writeTextGraph('Weights/my_model/saved_model.pb', 'Weights/')
+#
+# model = cv.dnn.readNetFromTensorflow('Weights/my_model.h5')
+#
+# img = cv.imread("Finale/Source/1538647057_1-2.jpg")
+#
+# #cv.imshow("1", img)
+#
+# # Use the given image as input, which needs to be blob(s).
+# img
+# blob = cv.dnn.blobFromImage(img, size=(720, 480), ddepth=cv.CV_32F, swapRB=True, crop=False)
+# model.setInput(blob)
+# # Runs a forward pass to compute the net output
+# out = model.forward()
+# print(1)
+# cv.imshow("3", out)
+#
+# cv.waitKey(0)
